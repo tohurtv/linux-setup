6d2829930e563e7093be8b16219f957c1f2fc947
tohurtv <joshwebb84@outlook.com>
2025-04-28 14:58:06 -0600

    new udev rules and additions to setup.sh

--- setup.sh
+++ setup.sh
@@ -44,4 +44,27 @@ for src in "${!DIRS[@]}"; do
   fi
 done
 
+#Enbale services
+systemctl enable lactd.service
+
+# rm .desktops
+rm /usr/share/applications/assistant.desktop 
+rm /usr/share/applications/avahi-discover.desktop 
+rm /usr/share/applications/bvnc.desktop
+rm /usr/share/applications/electron*.desktop 
+rm /usr/share/applications/*-openjdk.desktop 
+rm /usr/share/applications/linguist.desktop 
+rm /usr/share/applications/lstopo.desktop 
+rm /usr/share/applications/qdbusviewer.desktop 
+rm /usr/share/applications/qv4l2.desktop 
+rm /usr/share/applications/qvidcap.desktop 
+rm /usr/share/applications/stoken-*.desktop 
+rm /usr/share/applications/org.kde.kuserfeedback-console.desktop 
+rm /usr/share/applications/bssh.desktop 
+rm /../usr/share/applications/cups.desktop 
+rm /usr/share/applications/designer.desktop
+
+# create any needed symlinks
+ln -s /usr/lib/libopenh264.so /usr/lib/libopenh264.so.7
+
 echo "Installation complete."
