de400d0d960222697c9f0a66e4c32b36aad1e660
tohurtv <joshwebb84@outlook.com>
2025-04-29 14:46:55 -0600

    add aur user

--- setup.sh
+++ setup.sh
@@ -71,11 +71,16 @@ rm /usr/share/applications/designer.desktop
 # create any needed symlinks
 ln -s /usr/lib/libopenh264.so /usr/lib/libopenh264.so.7
 
+#temporaliy add aur user back
+useradd -m -G wheel -s /bin/bash aur
 #whitesur-gtk-theme
 mkdir -p /tmp/whitesur-gtk-theme-git
 curl -sSL https://raw.githubusercontent.com/$REPO/main/PKGBUILDS/whitesur-gtk-theme-git/PKGBUILD -o /tmp/whitesur-gtk-theme-git/PKGBUILD
 cd /tmp/whitesur-gtk-theme-git
 sudo -u aur makepkg -si --noconfirm
+
+#redelete aur user
+userdel -r aur
 
 # clean pacman cache
 rm -rf /var/cache/pacman
