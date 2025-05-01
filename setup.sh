5f1a7780cc4ffad03fadcefc0f62c66f37fc5693
Tohur <41875527+tohurtv@users.noreply.github.com>
2025-04-30 23:25:03 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -87,7 +87,7 @@ cd WhiteSur-icon-theme
 #userdel -r aur
 
 cd /tmp
-pacman -S --noconfirm cmake ninja eigen glew libpng lz4 bzip2 boost gtest opencv libuvc fmt python
+pacman -S --noconfirm cmake ninja eigen glew libpng lz4 bzip2 boost gtest opencv fmt python
 git clone --recursive https://gitlab.freedesktop.org/mateosss/basalt.git
 cd basalt
 cmake --preset library # use "development" instead of "library" if you want extra binaries and debug symbols
